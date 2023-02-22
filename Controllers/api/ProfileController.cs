8b46a26e4815f5d07a0676e410913ad666044cf9
Tayyab94 <tayyab.bhatti30@gmail.com>
2023-02-22 22:50:08 +0500

    inline input-tags datatable

--- Controllers/api/ProfileController.cs
+++ Controllers/api/ProfileController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using System;
 using System.Linq;
 using System.Linq.Dynamic.Core;
+using Application.Models.QueryParamModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Application.Controllers.api
 {
@@ -23,15 +25,16 @@ namespace Application.Controllers.api
         [HttpPost]
         public IActionResult GetProfiles([FromQuery] string name, string dt)
         {
+            string[] newSearches=name.Split(',');
             DateTime startDate = DateTime.Now;
             DateTime endDate = DateTime.Now;
             if (!string.IsNullOrEmpty(dt))
             {
                 string[] dates = dt.Split(' ');
-                 startDate = Convert.ToDateTime(dates[0]).Date.AddDays(1);
-                 endDate = Convert.ToDateTime(dates[1]).Date;
+                startDate = Convert.ToDateTime(dates[0]).Date;
+                endDate = Convert.ToDateTime(dates[1]).Date;
             }
-          
+
             try
             {
                 var draw = Request.Form["draw"].FirstOrDefault();
@@ -55,25 +58,45 @@ namespace Application.Controllers.api
                 //    customerData = customerData.Where(s => (s.Creation.Date <= startDate && s.Creation.Date >= endDate)).AsQueryable();
                 //}
 
-                if (!string.IsNullOrEmpty(name))
-                {
-                    customerData = customerData.Where(s => s.Tags.Contains(name.ToLower()) || s.Extension.Contains(name.ToLower()) || s.CallDuration.Value.Equals(name)).Where(s=>s.Creation.Date>= startDate.Date && s.Creation.Date<=endDate.Date).AsQueryable();
-
-                }
                 //if (!string.IsNullOrEmpty(name))
                 //{
-                //    customerData = customerData.Where(s =>  s.Tags.ToLower().Contains(name.ToLower()) || s.Extension.ToLower().Contains(name.ToLower()) ||
-                //                        s.CallDuration.Value.Equals(name) ||( s.CallSource.ToLower().Equals(name.ToLower()) && !string.IsNullOrEmpty(s.CallSource)) 
-                //                        || (s.CallDestination.ToLower().Equals(name.ToLower())&& !string.IsNullOrEmpty(s.CallDestination)));
-                    
+                //    customerData = customerData.Where(s => s.Tags.Contains(name.ToLower()) || s.Extension.Contains(name.ToLower()) || s.CallDuration.Value.Equals(name)).Where(s => s.Creation.Date >= startDate.Date && s.Creation.Date <= endDate.Date).AsQueryable();
+
                 //}
 
-             
+                if (!string.IsNullOrEmpty(newSearches[0]) || !string.IsNullOrEmpty(newSearches[1]) || !string.IsNullOrEmpty(newSearches[2]))
+                {
+                    customerData = customerData.Where(s => (s.Tags.ToLower().Contains(newSearches[0].ToLower()) 
+                                         && s.Extension.ToLower().Contains(newSearches[1].ToLower()) &&
+                    (
+                    s.CallSource.ToLower().Contains(newSearches[2].ToLower()) ||
+                    s.CallDestination.ToLower().Contains(newSearches[2].ToLower())))).Where(s => s.Creation.Date >= startDate.Date && s.Creation.Date <= endDate.Date).AsQueryable();
 
-                //if (!string.IsNullOrEmpty(searchValue))
+                }
+
+                //if (!string.IsNullOrEmpty(newSearches[0]) || !string.IsNullOrEmpty(newSearches[1]) || !string.IsNullOrEmpty(newSearches[2]))
                 //{
-                //    customerData = customerData.Where(s => s.Tags.Contains(searchValue.ToLower()) || s.Extension.Contains(searchValue.ToLower()) || s.CallDuration.Value.Equals(searchValue)|| s.CallSource.ToLower().Contains(searchValue.ToLower()) || s.CallDestination.ToLower().Contains(searchValue.ToLower()));
+                //    customerData = customerData.Where(s => newSearches[0].ToLower().Contains(s.Tags.ToLower()) && newSearches[1].ToLower().Contains(s.Extension.ToLower()) &&
+                //    (s.CallDuration.Value.Equals(Convert.ToDouble(newSearches[2])) || newSearches[2].ToLower().Contains(s.CallSource.ToLower()) || newSearches[2].ToLower().Contains(s.CallDestination.ToLower()))).Where(s => s.Creation.Date >= startDate.Date && s.Creation.Date <= endDate.Date).AsQueryable();
+
                 //}
+
+                //if (!string.IsNullOrEmpty(newSearches[0]))
+                //{
+                //    customerData = customerData.Where(s => newSearches[0].ToLower().Contains(s.Tags.ToLower())).AsQueryable();
+
+                //}
+                //if (!string.IsNullOrEmpty(newSearches[1]))
+                //{
+                //    customerData = customerData.Where(s =>  newSearches[1].ToLower().Contains(s.Extension.ToLower())).AsQueryable();
+
+                //}
+                //if (!string.IsNullOrEmpty(newSearches[2]))
+                //{
+                //    customerData = customerData.Where(s =>(s.CallDuration.Value.Equals(Convert.ToDouble(newSearches[2])) || newSearches[2].ToLower().Contains(s.CallSource.ToLower()) || newSearches[2].ToLower().Contains(s.CallDestination.ToLower()))).Where(s => s.Creation.Date >= startDate.Date && s.Creation.Date <= endDate.Date).AsQueryable();
+
+                //}
+
 
                 if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
                 {
@@ -84,8 +107,8 @@ namespace Application.Controllers.api
                     customerData = customerData.OrderByDescending(s => s.Creation).AsQueryable();
                 }
 
-                recordsTotal = customerData.Count();
-              
+                recordsTotal = customerData.ToList().Count();
+
                 var data = customerData.Skip(skip).Take(pageSize).ToList();
                 var jsonData = new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data };
                 return Ok(jsonData);
@@ -95,6 +118,160 @@ namespace Application.Controllers.api
                 throw;
             }
         }
+
+
+        //[HttpPost]
+        //[Consumes("application/json")]
+        //public IActionResult GetProfiles([FromBody] ProfileQueryViewModel model)
+        //{
+        //    DateTime startDate = DateTime.Now;
+        //    DateTime endDate = DateTime.Now;
+        //    if (!string.IsNullOrEmpty(model.Dt))
+        //    {
+        //        string[] dates = model.Dt.Split(' ');
+        //         startDate = Convert.ToDateTime(dates[0]).Date.AddDays(1);
+        //         endDate = Convert.ToDateTime(dates[1]).Date;
+        //    }
+
+        //    try
+        //    {
+        //        var draw = Request.Form["draw"].FirstOrDefault();
+        //        var start = Request.Form["start"].FirstOrDefault();
+        //        var length = Request.Form["length"].FirstOrDefault();
+        //        var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+        //        var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+        //        var searchValue = Request.Form["search[value]"].FirstOrDefault();
+        //        int pageSize = length != null ? Convert.ToInt32(length) : 0;
+        //        int skip = start != null ? Convert.ToInt32(start) : 0;
+        //        int recordsTotal = 0;
+        //        var customerData = (from tempcustomer in context.Profiles select tempcustomer).AsQueryable();
+
+
+
+        //        //List<ProfileViewModel> profiles = new List<ProfileViewModel>();
+
+        //        // var query = context.Profiles.AsQueryable();
+        //        //if (!string.IsNullOrEmpty(dt))
+        //        //{
+        //        //    customerData = customerData.Where(s => (s.Creation.Date <= startDate && s.Creation.Date >= endDate)).AsQueryable();
+        //        //}
+
+        //        if (!string.IsNullOrEmpty(model.Name))
+        //        {
+        //            customerData = customerData.Where(s => s.Tags.Contains(model.Name.ToLower()) || s.Extension.Contains(model.Name.ToLower()) || s.CallDuration.Value.Equals(model.Name)).Where(s=>s.Creation.Date>= startDate.Date && s.Creation.Date<=endDate.Date).AsQueryable();
+
+        //        }
+        //        //if (!string.IsNullOrEmpty(name))
+        //        //{
+        //        //    customerData = customerData.Where(s =>  s.Tags.ToLower().Contains(name.ToLower()) || s.Extension.ToLower().Contains(name.ToLower()) ||
+        //        //                        s.CallDuration.Value.Equals(name) ||( s.CallSource.ToLower().Equals(name.ToLower()) && !string.IsNullOrEmpty(s.CallSource)) 
+        //        //                        || (s.CallDestination.ToLower().Equals(name.ToLower())&& !string.IsNullOrEmpty(s.CallDestination)));
+
+        //        //}
+
+
+
+        //        //if (!string.IsNullOrEmpty(searchValue))
+        //        //{
+        //        //    customerData = customerData.Where(s => s.Tags.Contains(searchValue.ToLower()) || s.Extension.Contains(searchValue.ToLower()) || s.CallDuration.Value.Equals(searchValue)|| s.CallSource.ToLower().Contains(searchValue.ToLower()) || s.CallDestination.ToLower().Contains(searchValue.ToLower()));
+        //        //}
+
+        //        if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+        //        {
+        //            customerData = customerData.OrderBy(sortColumn + " " + sortColumnDirection).AsQueryable();
+        //        }
+        //        else
+        //        {
+        //            customerData = customerData.OrderByDescending(s => s.Creation).AsQueryable();
+        //        }
+
+        //        recordsTotal = customerData.Count();
+
+        //        var data = customerData.Skip(skip).Take(pageSize).ToList();
+        //        var jsonData = new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data };
+        //        return Ok(jsonData);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw;
+        //    }
+        //}
+
+        //[HttpPost]
+        //public IActionResult GetProfiles([FromQuery] string name, string dt, string tags, string extensions)
+        //{
+        //    DateTime startDate = DateTime.Now;
+        //    DateTime endDate = DateTime.Now;
+        //    if (!string.IsNullOrEmpty(dt))
+        //    {
+        //        string[] dates = dt.Split(' ');
+        //        startDate = Convert.ToDateTime(dates[0]).Date.AddDays(1);
+        //        endDate = Convert.ToDateTime(dates[1]).Date;
+        //    }
+
+        //    try
+        //    {
+        //        var draw = Request.Form["draw"].FirstOrDefault();
+        //        var start = Request.Form["start"].FirstOrDefault();
+        //        var length = Request.Form["length"].FirstOrDefault();
+        //        var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+        //        var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+        //        var searchValue = Request.Form["search[value]"].FirstOrDefault();
+        //        int pageSize = length != null ? Convert.ToInt32(length) : 0;
+        //        int skip = start != null ? Convert.ToInt32(start) : 0;
+        //        int recordsTotal = 0;
+        //        var customerData = (from tempcustomer in context.Profiles select tempcustomer).AsQueryable();
+
+
+
+        //        //List<ProfileViewModel> profiles = new List<ProfileViewModel>();
+
+        //        // var query = context.Profiles.AsQueryable();
+        //        //if (!string.IsNullOrEmpty(dt))
+        //        //{
+        //        //    customerData = customerData.Where(s => (s.Creation.Date <= startDate && s.Creation.Date >= endDate)).AsQueryable();
+        //        //}
+
+        //        if (!string.IsNullOrEmpty(name))
+        //        {
+        //            customerData = customerData.Where(s => s.Tags.Contains(name.ToLower()) || s.Extension.Contains(name.ToLower()) || s.CallDuration.Value.Equals(name)).Where(s => s.Creation.Date >= startDate.Date && s.Creation.Date <= endDate.Date).AsQueryable();
+
+        //        }
+        //        //if (!string.IsNullOrEmpty(name))
+        //        //{
+        //        //    customerData = customerData.Where(s =>  s.Tags.ToLower().Contains(name.ToLower()) || s.Extension.ToLower().Contains(name.ToLower()) ||
+        //        //                        s.CallDuration.Value.Equals(name) ||( s.CallSource.ToLower().Equals(name.ToLower()) && !string.IsNullOrEmpty(s.CallSource)) 
+        //        //                        || (s.CallDestination.ToLower().Equals(name.ToLower())&& !string.IsNullOrEmpty(s.CallDestination)));
+
+        //        //}
+
+
+
+        //        //if (!string.IsNullOrEmpty(searchValue))
+        //        //{
+        //        //    customerData = customerData.Where(s => s.Tags.Contains(searchValue.ToLower()) || s.Extension.Contains(searchValue.ToLower()) || s.CallDuration.Value.Equals(searchValue)|| s.CallSource.ToLower().Contains(searchValue.ToLower()) || s.CallDestination.ToLower().Contains(searchValue.ToLower()));
+        //        //}
+
+        //        if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+        //        {
+        //            customerData = customerData.OrderBy(sortColumn + " " + sortColumnDirection).AsQueryable();
+        //        }
+        //        else
+        //        {
+        //            customerData = customerData.OrderByDescending(s => s.Creation).AsQueryable();
+        //        }
+
+        //        recordsTotal = customerData.Count();
+
+        //        var data = customerData.Skip(skip).Take(pageSize).ToList();
+        //        var jsonData = new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data };
+        //        return Ok(jsonData);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw;
+        //    }
+        //}
 
         [HttpPost]
         public IActionResult GetProfiles1()
---
+++ Models/QueryParamModels/ProfileQueryViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Models.QueryParamModels
+{
+    public class ProfileQueryViewModel
+    {
+        public string Name { get; set; }
+        public string Dt { get; set; }
+
+        public string Tags { get; set; }
+        public string Extension { get; set; }
+     
+
+    }
+}
